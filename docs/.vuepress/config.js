e050eda5b30f7a15256ad0b42e2faf496991a21b
韩志伟 <hanzhiwei@lucidata.cn>
2021-09-14 17:40:06 +0800

    修改网站图标

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: 一尾流莺
  * @Description:配置文件
  * @Date: 2021-09-13 15:29:43
- * @LastEditTime: 2021-09-14 17:19:45
+ * @LastEditTime: 2021-09-14 17:30:41
  * @FilePath: \warblerjs-guide\docs\.vuepress\config.js
  */
 
@@ -10,10 +10,16 @@ const nav = require('./nav')
 const sidebar = require('./sidebar')
 
 module.exports = {
+  head: [
+    [
+      'link', // 设置 favicon.ico，注意图片放在 public 文件夹下
+      { rel: 'icon', href: '/images/favicon.png' }
+    ]
+  ],
   title: 'warbler-js',
   description: '一个 JavaScript 实用工具库',
   themeConfig: {
-    logo: '/images/main.jpg',
+    logo: '/images/favicon.png',
     nav,
     sidebar,
     sidebarDepth: 3
